32d89c1201760740be7a98a20325ee9fb2ac5581
John Walker <john.walker.dev@gmail.com>
2015-11-28 02:02:50 -0700

    chore(rel): quick

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -2,4 +2,5 @@
 
 # Remove the generated folder before each commit so it does not get added to the index
 # We can not gitignore the folder because travis-ci does not publish it to npm like its suppose to then
+
 rm -rf lib
